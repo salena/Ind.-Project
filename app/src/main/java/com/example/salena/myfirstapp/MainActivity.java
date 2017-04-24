dbf07a7049f557000b440ad601f76873a810e48c
salena <malhots1@tcnj.edu>
2017-04-24 18:26:23 -0400

    Fixed usb debugging issues, completed caluclator functionality

--- app/src/main/java/com/example/salena/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/salena/myfirstapp/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.salena.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
+/**
+ * The MainActivity of the system represents the starting point for the application.
+ * The only information that is made available to the Main Activity is the existence
+ *All other information for generating layouts and activities for the rest of the system is generated from the information
+ * given in the MainActivity class
+ */
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,11 +19,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button)findViewById(R.id.calculator);
-        Button button2 = (Button)findViewById(R.id.apartments);
-
-        button1.setOnClickListener(this);
+        Button button1 = (Button)findViewById(R.id.calculate); //create calculator button
+        Button button2 = (Button)findViewById(R.id.apartments); // create apartments button
+        Button button3 = (Button)findViewById(R.id.back);
+        button1.setOnClickListener(this); //sets button as onclick
         button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -26,15 +34,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onPause();
     }
 
-    public void onClick(View v){
+    public void onClick(View v){ //when the buttons are clicked where does it lead to
         Intent intent;
-                if(v.getId() == R.id.calculator) {
-                    intent = new Intent(this, calculator_activity.class);
-                    startActivity(intent);
-                } else if(v.getId() == R.id.apartments) {
-                    intent = new Intent(this, ApartmentActivity.class);
-                    startActivity(intent);
-                }
+        if(v.getId() == R.id.calculate) {
+            intent = new Intent(this, calculator_activity.class);
+            startActivity(intent);
+        } else if(v.getId() == R.id.apartments) {
+            intent = new Intent(this, ApartmentActivity.class);
+            startActivity(intent);
+        }
 
     }
 }
--- app/src/main/java/com/example/salena/myfirstapp/calculator_activity.java
+++ app/src/main/java/com/example/salena/myfirstapp/calculator_activity.java
@@ -1,33 +1,51 @@
 package com.example.salena.myfirstapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+
 
 public class calculator_activity extends AppCompatActivity implements View.OnClickListener {
     TextView input;
+    double salary;
+    double ira = 5500.0;
+    private TextView tv_result;
+    double monthly;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator_activity);
+        setContentView(R.layout.content_calculator_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         TextView input = (TextView)findViewById(R.id.salaryInput);
-
-
+        Button button3 = (Button)findViewById(R.id.back);
+        Button button4 = (Button)findViewById(R.id.calculate);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        tv_result = (TextView) findViewById(R.id.monthly);
     }
 
     public void onClick(View v) {
         Intent intent;
         if(v.getId() == R.id.calculate){
-            int salary = Integer.parseInt(input.getText().toString());
-            if(salary <)
-            calculate(Integer.parseInt (s));
+            salary = Double.parseDouble(((EditText)findViewById(R.id.salaryInput)).getText().toString());
+            System.out.print(salary);
+            if(salary >=10000)
+                tv_result.setText(String.valueOf((calculate(salary))));
 
+
+        else System.out.println("You don't make enough to meet this");
         }
         else if(v.getId() == R.id.back) {
             intent = new Intent(this, MainActivity.class);
@@ -35,9 +53,33 @@ public class calculator_activity extends AppCompatActivity implements View.OnCli
         }
     }
 
-    private int calculate(int salary) {
+    public double calculate(double salary) { //main calculation algorithm
+        double endSalary;
 
+        double taxDeduction = calculateTax(salary);
+        endSalary = (salary - ira - taxDeduction - 250);
+        monthly = endSalary/12;
+
+        return monthly;
     }
+
+    public double calculateTax(double salary) { //tax bracket calculation
+        double tax = 0;
+                if (salary <= 50000) {
+                    tax = salary * 0.01;
+                } else if (salary <= 75000) {
+                    tax = salary * 0.02;
+                } else if (salary <= 100000) {
+                    tax = salary * 0.03;
+                } else if (salary <= 250000) {
+                    tax = salary * 0.04;
+                } else if (salary <= 500000) {
+                    tax = salary * 0.05;
+                } else
+                    tax = salary * 0.06;
+            return tax;
+    }
+
 }
 
 
